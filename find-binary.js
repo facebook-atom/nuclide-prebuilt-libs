b0be642cf56c7e812f9c8d93d513ad7a6965ce35
hansonw <hansonw@fb.com>
2017-04-27 15:00:24 -0400

    Prebuild libraries for Electron 1.4 and 1.6
    A big thanks to @fmauNeko for the original contribution!! Closes #4.

--- find-binary.js
+++ find-binary.js
@@ -6,10 +6,22 @@ var path = require('path');
 module.exports = function findBinary(packageJsonPath) {
   var pack = JSON.parse(fs.readFileSync(packageJsonPath));
 
-  // ABI v49 is only for Electron. https://github.com/electron/electron/issues/5851
-  var nodeAbi = process.versions.modules === '49'
-    ? 'electron-v1.3'
-    : 'node-v' + process.versions.modules;
+  // ABI versions 49, 50, 53 are only for Electron.
+  // https://github.com/electron/electron/issues/5851
+  var nodeAbi;
+  switch (process.versions.modules) {
+    case '49':
+      nodeAbi = 'electron-v1.3';
+      break;
+    case '50':
+      nodeAbi = 'electron-v1.4';
+      break;
+    case '53':
+      nodeAbi = 'electron-v1.6';
+      break;
+    default:
+      nodeAbi = 'node-v' + process.versions.modules;
+  }
 
   var modulePath = pack.binary.module_path
     .replace('{module_name}', pack.binary.module_name)
--- prepublish.sh
+++ prepublish.sh
@@ -13,11 +13,15 @@ BINARY_HOST_MIRROR="https://github.com/facebooknuclide/nuclide-prebuilt-libs/rel
 
 TARGETS=(
   "--target_platform=linux --runtime=electron --target=1.3.6"
+  "--target_platform=linux --runtime=electron --target=1.4.16"
+  "--target_platform=linux --runtime=electron --target=1.6.5"
   "--target_platform=linux --runtime=node --target=5.0.0"
   "--target_platform=linux --runtime=node --target=6.0.0"
   "--target_platform=linux --runtime=node --target=7.0.0"
 
   "--target_platform=darwin --runtime=electron --target=1.3.6"
+  "--target_platform=darwin --runtime=electron --target=1.4.16"
+  "--target_platform=darwin --runtime=electron --target=1.6.5"
   "--target_platform=darwin --runtime=node --target=5.0.0"
   "--target_platform=darwin --runtime=node --target=6.0.0"
   "--target_platform=darwin --runtime=node --target=7.0.0"
