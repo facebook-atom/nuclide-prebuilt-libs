8096c38c142b7c62693ff1787879009791adec05
Konstantin Solomatov <solomatov@fb.com>
2019-11-16 13:44:34 -0800

    Remove diagnostics

--- fuzzy-native/lib/main.js
+++ fuzzy-native/lib/main.js
@@ -3,16 +3,4 @@
 var findBinary = require('../../find-binary');
 var binding_path = findBinary(require.resolve('../package.json'));
 
-console.log('Loading Native Lib!');
-console.log('path  = ' + binding_path);
-let nativeResult;
-try {
-    nativeResult = require(binding_path);
-    console.log('lib = ' + nativeResult);
-} catch (e) {
-    console.log('failed ', e);
-}
-
-
-
-module.exports = nativeResult
+module.exports = require(binding_path);
