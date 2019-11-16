992e8767e24787567eeaf7a8128270b2681e51a7
Konstantin Solomatov <solomatov@fb.com>
2019-11-16 12:48:14 -0800

    Fixing build

--- fuzzy-native/lib/main.js
+++ fuzzy-native/lib/main.js
@@ -3,10 +3,15 @@
 var findBinary = require('../../find-binary');
 var binding_path = findBinary(require.resolve('../package.json'));
 
-const nativeResult = require(binding_path);
-
 console.log('Loading Native Lib!');
 console.log('path  = ' + binding_path);
-console.log('lib = ' + nativeResult);
+try {
+    const nativeResult = require(binding_path);
+    console.log('lib = ' + nativeResult);
+} catch (e) {
+    console.log('failed ', e);
+}
+
+
 
 module.exports = nativeResult
